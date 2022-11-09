fcb800132430cb491c5161d27782eec7d763ab78
sb_lhj-at-021544330610 <vEfoNYgBF6ITOhPV17AO2fJDDWhtq1pnec/lRveaQMY=>
2022-11-09 17:05:00 +0900

    git commit

---
+++ src/test/java/com/jpastudy/MemberJpaTest.java
@@ -0,0 +1,29 @@
+package com.jpastudy;
+
+import com.jpastudy.domain.Member;
+import com.jpastudy.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberJpaTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void test(){
+        List<Member> resultList = memberRepository.findName("1");
+        for (Member rs:resultList) {
+            System.out.println(rs.getId()+":"+rs.getName());
+        }
+    }
+}
